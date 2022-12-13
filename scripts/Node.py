96e0df7a2eabdac8d75331ea8590764d64c6545d
vladi <vladimirrogozin21@gmail..com>
2022-12-13 21:11:50 +0300

    Added initial try of Body class

--- scripts/Node.py
+++ scripts/Node.py
@@ -3,5 +3,9 @@ import numpy as np
 
 class Node:
     def __init__(self):
-        self.sigma = np.zeros(3)
-        self.V = np.zeros(3)
+        self.sigma = np.zeros((), dtype=[('xx', np.float64), ('xy', np.float64), ('xz', np.float64),
+                                         ('yy', np.float64), ('yz', np.float64), ('zz', np.float64)])
+        self.V = np.zeros((), dtype=[('x', np.float64), ('y', np.float64), ('z', np.float64)])
+        self.mu = 0.5
+        self.rho = 1000
+        self.lmd = 0.5
