c4c4c3a6dbcdb7d97400fb30ec9c9816a76094ff
vladi <vladimirrogozin21@gmail..com>
2022-12-19 19:17:40 +0300

    Added garmonic tension function at the boundary and tried first calculation

--- scripts/Body.py
+++ scripts/Body.py
@@ -3,63 +3,69 @@ from scripts.Node import Node
 
 
 class Body:
-    def __init__(self, initdata, hx, hy, hz):
+    def __init__(self, initdata, materialdata, X, Y, Z):  # X, Y, Z -- длина, ширина, высота образца
         with open(initdata) as f:  # Считывание размера сетки из файла
             dims = np.array(f.readline().split(), dtype=int)
-        self.mp = np.array([[[Node() for i in range(dims[2])] for j in range(dims[1])] for k in range(dims[0])])  # Трёхмерная Сетка
+        with open(materialdata) as f:  # Считывание модулей среды
+            mods = np.array(f.readline().split(), dtype=float)
+        self.mp = np.array([[[Node(*mods) for i in range(dims[2])] for j in range(dims[1])] for k in range(dims[0])])  # Трёхмерная Сетка
         self.M, self.N, self.K = np.shape(self.mp)[2], np.shape(self.mp)[1], np.shape(self.mp)[0]  # Размеры сетки
-        self.h = np.array([hx, hy, hz], dtype=[('x', int), ('y', int), ('z', int)])  # Размеры ячеек
+        self.h = np.array([X / self.M, Y / self.N, Z / self.K], dtype=np.float64)  # Размеры ячеек
 
     def Velocity(self, PrevStep, dt):  # Функция вычисления скоростей, передаём в неё карту на предыдущей итерации
         for k in range(1, self.K - 1):  # Только внутренние точки
             for j in range(1, self.N - 1):
                 for i in range(1, self.M - 1):
                     # Вычисляем новое значение Vx
-                    self.mp[k, j, i].V['x'] += dt / self.mp[k, j, i].rho * \
-                            ((PrevStep[k, j, i + 1].sigma['xx'] - PrevStep[k, j, i - 1].sigma['xx']) / (2 * self.h['x']) +
-                            (PrevStep[k, j + 1, i].sigma['xy'] - PrevStep[k, j - 1, i].sigma['xy']) / (2 * self.h['y']) +
-                            (PrevStep[k + 1, j, i].sigma['xz'] - PrevStep[k - 1, j, i].sigma['xz']) / (2 * self.h['z']))
+                    self.mp[k, j, i].V[0] += dt / self.mp[k, j, i].rho * \
+                            ((PrevStep[k, j, i + 1].sigma[0] - PrevStep[k, j, i - 1].sigma[0]) / (2 * self.h[0]) +
+                            (PrevStep[k, j + 1, i].sigma[3] - PrevStep[k, j - 1, i].sigma[3]) / (2 * self.h[1]) +
+                            (PrevStep[k + 1, j, i].sigma[4] - PrevStep[k - 1, j, i].sigma[4]) / (2 * self.h[2]))
                     # Вычисляем новое значение Vy
-                    self.mp[k, j, i].V['y'] += dt / self.mp[k, j, i].rho * \
-                            ((PrevStep[k, j, i + 1].sigma['xy'] - PrevStep[k, j, i - 1].sigma['xy']) / (2 * self.h['x']) +
-                            (PrevStep[k, j + 1, i].sigma['yy'] - PrevStep[k, j - 1, i].sigma['yy']) / (2 * self.h['y']) +
-                            (PrevStep[k + 1, j, i].sigma['zy'] - PrevStep[k - 1, j, i].sigma['zy']) / (2 * self.h['z']))
+                    self.mp[k, j, i].V[1] += dt / self.mp[k, j, i].rho * \
+                            ((PrevStep[k, j, i + 1].sigma[3] - PrevStep[k, j, i - 1].sigma[3]) / (2 * self.h[0]) +
+                            (PrevStep[k, j + 1, i].sigma[1] - PrevStep[k, j - 1, i].sigma[1]) / (2 * self.h[1]) +
+                            (PrevStep[k + 1, j, i].sigma[5] - PrevStep[k - 1, j, i].sigma[5]) / (2 * self.h[2]))
                     # Вычисляем новое значение Vz
-                    self.mp[k, j, i].V['z'] += dt / self.mp[k, j, i].rho * \
-                            ((PrevStep[k, j, i + 1].sigma['xz'] - PrevStep[k, j, i - 1].sigma['xz']) / (2 * self.h['x']) +
-                            (PrevStep[k, j + 1, i].sigma['yz'] - PrevStep[k, j - 1, i].sigma['yz']) / (2 * self.h['y']) +
-                            (PrevStep[k + 1, j, i].sigma['zz'] - PrevStep[k - 1, j, i].sigma['zz']) / (2 * self.h['z']))
+                    self.mp[k, j, i].V[2] += dt / self.mp[k, j, i].rho * \
+                            ((PrevStep[k, j, i + 1].sigma[4] - PrevStep[k, j, i - 1].sigma[4]) / (2 * self.h[0]) +
+                            (PrevStep[k, j + 1, i].sigma[5] - PrevStep[k, j - 1, i].sigma[5]) / (2 * self.h[1]) +
+                            (PrevStep[k + 1, j, i].sigma[2] - PrevStep[k - 1, j, i].sigma[2]) / (2 * self.h[2]))
 
     def Tension(self, PrevStep, dt):  # Функция вычисления напряжений, передаём в неё карту на предыдущей итерации
-        for k in range(self.K - 1):  # Только внутренние точки
-            for j in range(self.N - 1):
-                for i in range(self.M - 1):
+        for k in range(1, self.K - 1):  # Только внутренние точки
+            for j in range(1, self.N - 1):
+                for i in range(1, self.M - 1):
                     # Вычисляем новое значение напряжения xx
-                    self.mp[k, j, i].sigma['xx'] += dt * ((self.mp[k, j, i].lmd + 2 * self.mp[k, j, i].mu) *
-                        (PrevStep[k, j, i + 1].V['x'] - PrevStep[k, j, i - 1].V['x']) / (2 * self.h['x']) +
-                        self.mp[k, j, i].lmd * (PrevStep[k, j + 1, i].V['y'] - PrevStep[k, j - 1, i].V['y']) / (2 * self.h['y']) +
-                        self.mp[k, j, i].lmd * (PrevStep[k + 1, j, i].V['z'] - PrevStep[k - 1, j, i].V['z']) / (2 * self.h['z']))
+                    self.mp[k, j, i].sigma[0] += dt * ((self.mp[k, j, i].lmd + 2 * self.mp[k, j, i].mu) *
+                        (PrevStep[k, j, i + 1].V[0] - PrevStep[k, j, i - 1].V[0]) / (2 * self.h[0]) +
+                        self.mp[k, j, i].lmd * (PrevStep[k, j + 1, i].V[1] - PrevStep[k, j - 1, i].V[1]) / (2 * self.h[1]) +
+                        self.mp[k, j, i].lmd * (PrevStep[k + 1, j, i].V[2] - PrevStep[k - 1, j, i].V[2]) / (2 * self.h[2]))
                     # Вычисляем новое значение напряжения yy
-                    self.mp[k, j, i].sigma['yy'] += dt * ((self.mp[k, j, i].lmd + 2 * self.mp[k, j, i].mu) *
-                        (PrevStep[k, j + 1, i].V['y'] - PrevStep[k, j - 1, i].V['y']) / (2 * self.h['y']) +
-                        self.mp[k, j, i].lmd * (PrevStep[k, j, i + 1].V['x'] - PrevStep[k, j, i - 1].V['x']) / (2 * self.h['x']) +
-                        self.mp[k, j, i].lmd * (PrevStep[k + 1, j, i].V['z'] - PrevStep[k - 1, j, i].V['z']) / (2 * self.h['z']))
-                    # Вычисляем новое значение напряжения yy
-                    self.mp[k, j, i].sigma['zz'] += dt * ((self.mp[k, j, i].lmd + 2 * self.mp[k, j, i].mu) *
-                        (PrevStep[k + 1, j, i].V['z'] - PrevStep[k - 1, j, i].V['z']) / (2 * self.h['z']) +
-                        self.mp[k, j, i].lmd * (PrevStep[k, j + 1, i].V['y'] - PrevStep[k, j - 1, i].V['y']) / (2 * self.h['y']) +
-                        self.mp[k, j, i].lmd * (PrevStep[k, j, i + 1].V['z'] - PrevStep[k, j, i - 1].V['x']) / (2 * self.h['x']))
+                    self.mp[k, j, i].sigma[1] += dt * ((self.mp[k, j, i].lmd + 2 * self.mp[k, j, i].mu) *
+                        (PrevStep[k, j + 1, i].V[1] - PrevStep[k, j - 1, i].V[1]) / (2 * self.h[1]) +
+                        self.mp[k, j, i].lmd * (PrevStep[k, j, i + 1].V[0] - PrevStep[k, j, i - 1].V[0]) / (2 * self.h[0]) +
+                        self.mp[k, j, i].lmd * (PrevStep[k + 1, j, i].V[2] - PrevStep[k - 1, j, i].V[2]) / (2 * self.h[2]))
+                    # Вычисляем новое значение напряжения zz
+                    self.mp[k, j, i].sigma[2] += dt * ((self.mp[k, j, i].lmd + 2 * self.mp[k, j, i].mu) *
+                        (PrevStep[k + 1, j, i].V[2] - PrevStep[k - 1, j, i].V[2]) / (2 * self.h[2]) +
+                        self.mp[k, j, i].lmd * (PrevStep[k, j + 1, i].V[1] - PrevStep[k, j - 1, i].V[1]) / (2 * self.h[1]) +
+                        self.mp[k, j, i].lmd * (PrevStep[k, j, i + 1].V[0] - PrevStep[k, j, i - 1].V[0]) / (2 * self.h[0]))
                     # Вычисляем новое значение напряжения xy
-                    self.mp[k, j, i].sigma['xy'] += dt * ((PrevStep[k, j, i + 1].V['y'] - PrevStep[k, j, i - 1].V['y']) / (2 * self.h['x'])
-                                                          + (PrevStep[k, j + 1, i].V['x'] - PrevStep[k, j - 1, i].V['x']) / (2 * self.h['y'])) * \
+                    self.mp[k, j, i].sigma[3] += dt * ((PrevStep[k, j, i + 1].V[1] - PrevStep[k, j, i - 1].V[1]) / (2 * self.h[0])
+                                                          + (PrevStep[k, j + 1, i].V[0] - PrevStep[k, j - 1, i].V[0]) / (2 * self.h[1])) * \
                                                     self.mp[k, j, i].mu
                     # Вычисляем новое значение напряжения xz
-                    self.mp[k, j, i].sigma['xz'] += dt * ((PrevStep[k, j, i + 1].V['z'] - PrevStep[k, j, i - 1].V['z']) / (2 * self.h['x'])
-                                                          + (PrevStep[k + 1, j, i].V['x'] - PrevStep[k - 1, j, i].V['x']) / (2 * self.h['z'])) * \
+                    self.mp[k, j, i].sigma[4] += dt * ((PrevStep[k, j, i + 1].V[2] - PrevStep[k, j, i - 1].V[2]) / (2 * self.h[0])
+                                                          + (PrevStep[k + 1, j, i].V[0] - PrevStep[k - 1, j, i].V[0]) / (2 * self.h[2])) * \
                                                     self.mp[k, j, i].mu
 
                     # Вычисляем новое значение напряжения yz
-                    self.mp[k, j, i].sigma['yz'] += dt * ((PrevStep[k + 1, j, i].V['y'] - PrevStep[k - 1, j, i].V['y']) / (2 * self.h['z'])
-                                                          + (PrevStep[k, j + 1, i].V['z'] - PrevStep[k, j - 1, i].V['z']) / (2 * self.h['y'])) * \
+                    self.mp[k, j, i].sigma[5] += dt * ((PrevStep[k + 1, j, i].V[1] - PrevStep[k - 1, j, i].V[1]) / (2 * self.h[2])
+                                                          + (PrevStep[k, j + 1, i].V[2] - PrevStep[k, j - 1, i].V[2]) / (2 * self.h[1])) * \
                                                     self.mp[k, j, i].mu
 
+    def GarmonicBorderTension(self, A, omega, t): # Случай изменения напряжений на одной из границ по гармоническому закону
+        for j in range(1, self.N - 1):
+            for i in range(1, self.M - 1):
+                self.mp[0, j, i].sigma[2] = A * np.cos(omega * t)  # Задаём нормальное напряжение на одной грани, ортог oz
--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,27 @@
 from scripts.Body import Body
+import copy
+
+c = 3500  # Примерная скорость звука в меди в метрах на секунду
+A, B, C = 1, 1, 1  # Длина, ширина, высота образца в метрах
+N = 200  # Количество итераций
 
 # Сетка
-Body = Body('init_data.txt', 1, 1, 1)
-print(Body)
-print(Body.mp[0, 0, 0])
+Body = Body('init_data.txt', 'material_data.txt', A, B, C)
 
-# Здесь должно задаваться начальное напряжение
+dt = 0.5 * Body.h[0] / c  # Шаг по времени в секундах
+omega = c / Body.h[0] * 0.5  # Частота гармонического напряжения
+Ampl = 1000  # Амлитуда гармонического напряжения в ньютонах на метр
+
+for i in range(N):
+    PrevStep = copy.deepcopy(Body.mp)
+    Body.GarmonicBorderTension(Ampl, omega, i * dt)
+    Body.Velocity(PrevStep, dt)
+    Body.Tension(PrevStep, dt)
+    # Смотрим за значениями в клетках
+    print(Body.mp[10, 10, 10].V[0], Body.mp[10, 10, 10].V[1], Body.mp[10, 10, 10].V[2])
+    for u in Body.mp[10, 10, 10].sigma:
+        print(u, end=' ')
+    print()
+    for u in Body.mp[11, 10, 10].sigma:
+        print(u, end=' ')
+    print()
